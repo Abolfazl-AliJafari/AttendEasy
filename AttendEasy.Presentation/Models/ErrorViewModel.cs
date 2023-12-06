7300f086c40a89fce43bae756378bf5f60ebbf5f
Abolfazl Alijafari <134207588+Abolfazl-AliJafari-Taheri@users.noreply.github.com>
2023-12-06 19:25:47 +0330

    Add project files.

---
+++ AttendEasy.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AttendEasy.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
